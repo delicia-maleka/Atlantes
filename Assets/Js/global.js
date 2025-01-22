fd28fb5b5504bad09385cef8b93937d33cb63139
MaelPerot <mael.perot18@gmail.com>
2025-01-22 20:57:09 +0100

    feat: slider slides within limits without bar

--- Assets/Js/global.js
+++ Assets/Js/global.js
@@ -1,1 +1,26 @@
-sliderImagesWidth = document.getElementById("sliderImages").style.width;
+var sliderImageWidth = document.getElementById("basicImg").offsetWidth;
+var maxSliderOffset = -7 * sliderImageWidth;
+
+
+const slider = document.querySelector('.slideContainer');
+const sliderImages = document.querySelector('.sliderCards');
+let mouseMaintained = false;
+let cursorX;
+
+slider.addEventListener("mousedown", (e) => {
+    mouseMaintained = true;
+    cursorX = e.offsetX - sliderImages.offsetLeft;
+});
+
+slider.addEventListener("mousemove", (e) => {
+    if (!mouseMaintained)
+        return;
+
+    e.preventDefault();
+    if ((e.offsetX - cursorX >= maxSliderOffset) && (e.offsetX - cursorX <= 0))
+        sliderImages.style.left = `${e.offsetX - cursorX}px`;
+});
+
+window.addEventListener("mouseup", () => {
+    mouseMaintained = false;
+});
