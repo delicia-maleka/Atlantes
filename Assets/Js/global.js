6d1b9668505adfe999cb31ac4d175719cee8fddc
MaelPerot <mael.perot18@gmail.com>
2025-01-23 17:59:11 +0100

    feat: slide and bar working properly

--- Assets/Js/global.js
+++ Assets/Js/global.js
@@ -1,15 +1,17 @@
 var sliderImageWidth = document.getElementById("basicImg").offsetWidth;
-var maxSliderOffset = -7 * sliderImageWidth;
+var sliderImagesMargin = document.getElementById("basicImg").style.marginRight;
+var maxSliderOffset = -9 * sliderImageWidth;
 
-
+const bars = document.querySelectorAll('.bar');
 const slider = document.querySelector('.slideContainer');
-const sliderImages = document.querySelector('.sliderCards');
+const sliderCards = document.querySelector('.sliderCards');
 let mouseMaintained = false;
 let cursorX;
 
+/* slider grab */
 slider.addEventListener("mousedown", (e) => {
     mouseMaintained = true;
-    cursorX = e.offsetX - sliderImages.offsetLeft;
+    cursorX = e.offsetX - sliderCards.offsetLeft;
 });
 
 slider.addEventListener("mousemove", (e) => {
@@ -17,10 +19,25 @@ slider.addEventListener("mousemove", (e) => {
         return;
 
     e.preventDefault();
-    if ((e.offsetX - cursorX >= maxSliderOffset) && (e.offsetX - cursorX <= 0))
-        sliderImages.style.left = `${e.offsetX - cursorX}px`;
+    if ((e.offsetX - cursorX >= maxSliderOffset) && (e.offsetX - cursorX <= 0)) {
+        sliderCards.style.left = `${e.offsetX - cursorX}px`;
+        bars.forEach((bar) => {
+            bar.classList.remove('highlightBar');
+        });
+        document.getElementById("bar"+Math.round((- e.offsetX + cursorX) / sliderImageWidth)).classList.add('highlightBar');
+    }
 });
 
 window.addEventListener("mouseup", () => {
     mouseMaintained = false;
 });
+
+
+/* slider bar */
+function firstSlide(slideIndex) {
+    sliderCards.style.left = `-${sliderImageWidth * slideIndex + sliderImagesMargin}px`;
+    bars.forEach((bar) => {
+        bar.classList.remove('highlightBar');
+    });
+    document.getElementById("bar"+slideIndex).classList.add('highlightBar');
+}
